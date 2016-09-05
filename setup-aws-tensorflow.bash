94a5ed3a357a158ceb9bf974f2afb77f22fdbc84
Jeremy Heffner <jerheff@gmail.com>
2016-09-05 13:39:22 -0400

    Switch to Python 2 & update TF

--- setup-aws-tensorflow.bash
+++ setup-aws-tensorflow.bash
@@ -36,15 +36,15 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
 ' >> ~/.bashrc
 
 # install anaconda
-wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
-bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p /mnt/bin/anaconda3
-rm Anaconda3-4.0.0-Linux-x86_64.sh
-echo 'export PATH="/mnt/bin/anaconda3/bin:$PATH"' >> ~/.bashrc
+wget https://repo.continuum.io/archive/Anaconda2-4.1.1-MacOSX-x86_64.sh
+bash Anaconda2-4.1.1-MacOSX-x86_64.sh -b -p /mnt/bin/anaconda2
+rm Anaconda2-4.1.1-MacOSX-x86_64.sh
+echo 'export PATH="/mnt/bin/anaconda2/bin:$PATH"' >> ~/.bashrc
 
 # install tensorflow
-export TF_BINARY_URL='https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl'
+export TF_BINARY_URL='https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl'
 
-/mnt/bin/anaconda3/bin/pip install $TF_BINARY_URL
+/mnt/bin/anaconda2/bin/pip install $TF_BINARY_URL
 
 # install monitoring programs
 sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
